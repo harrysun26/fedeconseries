3c4972e45a5ec8fce44d05e37f38c60c4016b5f4
harrysun26 <harrysunclick@gmail.com>
2020-02-01 15:45:26 -0800

    first commit

---
+++ R/hello.R
@@ -0,0 +1,24 @@
+#' @title Federal Reserve Econnomic Series Data
+#'
+#' @description To obtain US federal reserve economc data.  Reserach use only.
+#'
+#' @param x the input, the name you want to say hello to
+#'
+#' @return the output from \code{\link{print}}
+#'
+#' @export
+#'
+#' @examples
+#' hello("John")
+#' \dontrun{
+#' hello("Steve")
+#' }
+#'
+#'
+#'
+#'
+#'
+hello <- function(x) {
+  print(paste("Hello",x,"welcome" ))
+}
+
