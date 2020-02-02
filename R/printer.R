4cf0ab217e1846ef048b6e6939ccbc28bcf42488
harrysun26 <harrysunclick@gmail.com>
2020-02-01 17:11:20 -0800

    add vignettes

--- R/printer.R
+++ R/printer.R
@@ -1,14 +1,22 @@
 
-#' Title
+#' printer dunction
 #'
-#' @param r
-#' @param x
-#' @param y
 #'
-#' @return
+#' @param r the second column
+#' @param x the first column
+#'
+#'
+#' @return a tibble
 #' @export
 #'
+#' @importFrom tibble data_frame
+#' @importFrom utils head
+#'
 #' @examples
-printer <- function(r,x,y){
-  print(paste0("x=",x))
+#' printer(x=rnorm(5), r=rnorm(5))
+printer <- function(r,x){
+  x = data_frame(x=x,r=r)
+  print(head(x))
+  # print(paste0("x=",x))
+  return(x)
 }
