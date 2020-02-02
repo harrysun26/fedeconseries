a614d7de5f9cd17def4172653e0c31d08f1c663b
harrysun26 <harrysunclick@gmail.com>
2020-02-01 21:21:25 -0800

    df

--- R/printer.R
+++ R/printer.R
@@ -9,13 +9,13 @@
 #' @return a tibble
 #' @export
 #'
-#' @importFrom tibble data_frame
+#' @importFrom tibble as_data_frame
 #' @importFrom utils head
 #'
 #' @examples
 #' printer(x=rnorm(5), r=rnorm(5))
 printer <- function(r,x){
-  x = data_frame(x=x,r=r)
+  x = as_data_frame(x=x,r=r)
   print(head(x))
   # print(paste0("x=",x))
   return(x)
