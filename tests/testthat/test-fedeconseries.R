08547dfee27135489ef4dbf8a92e3e7cc8b32f9b
harrysun26 <harrysunclick@gmail.com>
2020-02-02 00:49:23 -0800

    fix test

--- tests/testthat/test-fedeconseries.R
+++ tests/testthat/test-fedeconseries.R
@@ -1,11 +1,37 @@
-context("just testing fedeconseries functionality")
-
 testthat::test_that("whether the function can connect and return values",{
-  set.seed(1)
-  res = get_fedeconseries("INDPRO")
-  print(class(df_series))
-  testthat::expect_equal(class(df_series),"try-error")
-  testthat::expect_equal(class(df_series),"data.frame")
-  testthat::expect_equal(class(df_series),"data.frame")
-
+  testthat::expect_equal(
+    get_fedeconseries("INDPRO", "test",
+                      obs_start = "2013-03-01", obs_end = "2013-03-01",
+                      real_start = "2015-02-02", real_end = "2015-02-02"),
+    data.frame(
+      date = as.Date("2013-03-01"),
+      realtime_period  = as.Date("2015-02-02"),
+      test = 99.488)
+  )
+  testthat::expect_equal(
+    dplyr::filter(get_fedeconseries("INDPRO", "test",
+                                    real_start = "2015-02-02", real_end = "2015-02-02"),
+                  date == "2013-03-01"),
+    data.frame(
+      date = as.Date("2013-03-01"),
+      realtime_period  = as.Date("2015-02-02"),
+      test = 99.488)
+  )
+  testthat::expect_equal(
+    dplyr::filter(get_fedeconseries("INDPRO", "test",
+                                    obs_start = "2009-03-01", obs_end = "2009-03-01"),
+                  realtime_period == "2015-02-18"),
+    data.frame(
+      date = as.Date("2009-03-01"),
+      realtime_period = as.Date("2015-02-18"),
+      test = 85.6157)
+  )
+  testthat::expect_error(
+    get_fedeconseries(1231232, "test",
+                    obs_start = "2009-03-01", obs_end = "2009-03-01")
+  )
+  testthat::expect_error(
+    get_fedeconseries(1231232, "test",
+                      obs_start = "2009-03-01", obs_end = "2009-03-01")
+  )
 })
